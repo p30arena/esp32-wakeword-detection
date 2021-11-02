a9965cd54689352e6745799e947035c7fdee52ec
Ali EP <p30arena@gmail.com>
2021-11-02 23:03:02 +0330

    add windowing to the test loop

--- esp32-sampling-i2s/test/test_loop.py
+++ esp32-sampling-i2s/test/test_loop.py
@@ -6,12 +6,32 @@ from time import sleep
 from commons import freq
 
 from model import get_model_test, get_spectrogram
-from train import AUTOTUNE, commands
 
 i = 0
 closed = False
 duration = 1  # seconds
 model = get_model_test()
+last_half = []
+
+
+def infer(frame_data, is_mid=False):
+    spectogram = get_spectrogram(frame_data.astype(np.float32) / 32768)
+    spectogram = spectogram[None, :]
+    prediction = model.predict(spectogram)
+    sm = tf.nn.softmax(prediction[0])
+    idx = np.argmax(sm)
+
+    if idx == 1 and not is_mid:
+        print("{0} nothing happening here".format(i))
+    elif idx == 0:
+        print('\nprobability: {0}'.format(sm[0]))
+        print("I'm at your service!")
+        print("abreman.ir")
+        print('')
+        sleep(3)
+        return True
+    return False
+
 
 while not closed:
     try:
@@ -20,21 +40,12 @@ while not closed:
         sd.wait()
         frame_data = frame_data.flatten()
         frame_data += 22000
-
-        spectogram = get_spectrogram(frame_data.astype(np.float32) / 32768)
-        spectogram = spectogram[None, :]
-        prediction = model.predict(spectogram)
-        sm = tf.nn.softmax(prediction[0])
-        idx = np.argmax(sm)
-
-        if idx == 1:
-            print("{0} nothing happening here".format(i))
-        else:
-            print('\nprobability: {0}'.format(sm[0]))
-            print("I'm at your service!")
-            print("abreman.ir")
-            print('')
-            sleep(3)
+        mid_ok = False
+        if len(last_half) > 0:
+            mid_ok = infer(last_half + frame_data[:8000], is_mid=True)
+        if not mid_ok:
+            infer(frame_data)
+        last_half = frame_data[8000:]
         i += 1
     except KeyboardInterrupt:
         closed = True
