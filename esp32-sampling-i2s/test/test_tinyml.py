0b9fa63e6f4f9c947c92d1978b60f4d1215c976c
Ali EP <p30arena@gmail.com>
2021-11-04 08:42:22 +0330

    working!

--- esp32-sampling-i2s/test/test_tinyml.py
+++ esp32-sampling-i2s/test/test_tinyml.py
@@ -2,21 +2,21 @@ import matplotlib.pyplot as plt
 import sounddevice as sd
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.ops import math_ops
 
 from commons import freq
-
-from model import get_model_test_tinyml, get_spectrogram
+from model import get_model_test_tinyml, get_spectrogram, decode_audio
 from train import commands
 
 print("SPEAK!")
 
-frame_data = sd.rec(1 * freq, samplerate=freq,
-                    channels=1, dtype=np.int16)
-sd.wait()
+# frame_data = sd.rec(1 * freq, samplerate=freq,
+#                     channels=1, dtype=np.int16)
+# sd.wait()
 # sd.play(frame_data, freq)
 # sd.wait()
-frame_data = frame_data.flatten()
-frame_data += 22000
+# frame_data = frame_data.flatten()
+# frame_data += 22000
 
 model = get_model_test_tinyml()
 model.allocate_tensors()
@@ -24,19 +24,23 @@ input_details = model.get_input_details()
 output_details = model.get_output_details()
 input_shape = input_details[0]['shape']
 
-spectrogram = get_spectrogram(frame_data.astype(np.float32) / 32768)
-# spectrogram = tf.cast(spectrogram * 2147483648, tf.int8)
-# spectrogram = tf.cast(spectrogram * 32768, tf.int8)
-# spectrogram = tf.cast(spectrogram * 255, tf.int8)
-spectrogram = tf.image.convert_image_dtype(spectrogram, dtype=tf.int8)
-spectrogram = spectrogram[np.newaxis, ..., np.newaxis]
-model.set_tensor(input_details[0]['index'], spectrogram)
-model.invoke()
+for i in range(80):
+    audio_binary = tf.io.read_file("out/data/cmd-1/{0}.wav".format(i))
+    decoded_audio = decode_audio(audio_binary)
+    spectrogram = get_spectrogram(decoded_audio)
+    # spectrogram = get_spectrogram(frame_data.astype(np.float32) / 32768)
+    spectrogram = tf.cast(spectrogram * 128 - 128, tf.int8)
+    spectrogram = spectrogram[np.newaxis, ..., np.newaxis]
+    model.set_tensor(input_details[0]['index'], spectrogram)
+    model.invoke()
 
-output_data = model.get_tensor(output_details[0]['index'])
-print(output_data)
-output_data = output_data[0].astype(np.float) / 255
-print(output_data)
+    output_data = model.get_tensor(output_details[0]['index'])
+    # print(tf.argmax(tf.nn.softmax(output_data[0])).numpy())
+    idx = tf.argmax(tf.nn.softmax(
+        output_data[0].astype(np.float) / 128)).numpy()
+    print(idx)
+    # output_data = output_data[0].astype(np.float) / 128
+    # print(output_data)
 
-plt.bar(commands, tf.nn.softmax(output_data))
-plt.show()
+    # plt.bar(commands, tf.nn.softmax(output_data))
+    # plt.show()
