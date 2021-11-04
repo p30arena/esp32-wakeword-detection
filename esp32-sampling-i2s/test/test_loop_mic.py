719154e83de9618bf9c021a19f84f36b788b2f5f
Ali EP <p30arena@gmail.com>
2021-11-04 09:07:16 +0330

    more tests

--- esp32-sampling-i2s/test/test_loop_mic.py
+++ esp32-sampling-i2s/test/test_loop_mic.py
@@ -1,9 +1,10 @@
+import sounddevice as sd
 import numpy as np
 import tensorflow as tf
 from time import sleep
 
 from commons import freq
-import capture
+
 from model import get_model_test, get_spectrogram
 
 i = 0
@@ -11,13 +12,10 @@ closed = False
 duration = 1  # seconds
 model = get_model_test()
 last_half = []
-n_captured = 0
-data = []
 
 
 def infer(frame_data, is_mid=False):
-    frame_data = np.array(frame_data, dtype=np.float32) / 32768
-    spectrogram = get_spectrogram(frame_data)
+    spectrogram = get_spectrogram(frame_data.astype(np.float32) / 32768)
     spectrogram = spectrogram[None, :]
     prediction = model.predict(spectrogram)
     sm = tf.nn.softmax(prediction[0])
@@ -35,28 +33,19 @@ def infer(frame_data, is_mid=False):
     return False
 
 
-def on_connected():
-    print("CONNECTED!")
-
-
-def on_frame(frame_data: bytes, num_data: list):
-    global n_captured, data, i, last_half
-    n_captured += 1
-
-    data += num_data
-
-    if n_captured % 2 == 0:
+while not closed:
+    try:
+        frame_data = sd.rec(duration * freq, samplerate=freq,
+                            channels=1, dtype=np.int16)
+        sd.wait()
+        frame_data = frame_data.flatten()
+        frame_data += 22000
         mid_ok = False
         if len(last_half) > 0:
-            mid_ok = infer(last_half + data[:8000], is_mid=True)
+            mid_ok = infer(last_half + frame_data[:8000], is_mid=True)
         if not mid_ok:
-            infer(data)
-        last_half = data[8000:]
+            infer(frame_data)
+        last_half = frame_data[8000:]
         i += 1
-        data = []
-    else:
-        return
-
-
-while capture.loop(on_connected, on_frame) == 1:
-    print("restarting")
+    except KeyboardInterrupt:
+        closed = True
