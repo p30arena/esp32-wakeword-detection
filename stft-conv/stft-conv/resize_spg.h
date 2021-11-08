f14ec8bd54dd2d766233f33f78d3d9d70b2dae9b
Ali EP <p30arena@gmail.com>
2021-11-08 19:12:23 +0330

    hmmm

--- stft-conv/stft-conv/resize_spg.h
+++ stft-conv/stft-conv/resize_spg.h
@@ -8,21 +8,21 @@ typedef struct
 } image_t;
 #define getByte(value, n) (float)(value >> (n * 8) & 0xFF)
 
-uint32_t getpixel(image_t *image, unsigned int x, unsigned int y)
+double getpixel(image_t *image, unsigned int x, unsigned int y)
 {
-	return (uint32_t)(image->pixels[(y * image->w) + x] * 256);
+	return image->pixels[(y * image->w) + x];
 }
 
 float max(float a, float b) { return (a < b) ? a : b; };
-float lerp(float s, float e, float t) { return s + (e - s) * t; }
-float blerp(float c00, float c10, float c01, float c11, float tx, float ty)
+double lerp(double s, double e, double t) { return s + (e - s) * t; }
+double blerp(double c00, double c10, double c01, double c11, double tx, double ty)
 {
 	return lerp(lerp(c00, c10, tx), lerp(c01, c11, tx), ty);
 }
 
-void putpixel(image_t *image, unsigned int x, unsigned int y, uint32_t color)
+void putpixel(image_t *image, unsigned int x, unsigned int y, double color)
 {
-	image->pixels[(y * image->w) + x] = color / 256.0f;
+	image->pixels[(y * image->w) + x] = color;
 }
 
 void resize(image_t *src, image_t *dst, int newWidth, int newHeight)
@@ -41,17 +41,18 @@ void resize(image_t *src, image_t *dst, int newWidth, int newHeight)
 		float gy = max(y / (float)((newHeight) * (src->h) - 0.5f), (float)(src->h - 1));
 		int gxi = (int)gx;
 		int gyi = (int)gy;
-		uint32_t result = 0;
-		uint32_t c00 = getpixel(src, gxi, gyi);
-		uint32_t c10 = getpixel(src, gxi + 1, gyi);
-		uint32_t c01 = getpixel(src, gxi, gyi + 1);
-		uint32_t c11 = getpixel(src, gxi + 1, gyi + 1);
+		double result = 0;
+		double c00 = getpixel(src, gxi, gyi);
+		double c10 = getpixel(src, gxi + 1, gyi);
+		double c01 = getpixel(src, gxi, gyi + 1);
+		double c11 = getpixel(src, gxi + 1, gyi + 1);
 
-		uint8_t i;
-		for (i = 0; i < 3; i++)
-		{
-			result |= (uint8_t)blerp(getByte(c00, i), getByte(c10, i), getByte(c01, i), getByte(c11, i), gx - gxi, gy - gyi) << (8 * i);
-		}
+		//uint8_t i;
+		//for (i = 0; i < 3; i++)
+		//{
+		//	result |= (uint8_t)blerp(getByte(c00, i), getByte(c10, i), getByte(c01, i), getByte(c11, i), gx - gxi, gy - gyi) << (8 * i);
+		//}
+		result = blerp(c00, c10, c01, c11, gx - gxi, gy - gyi);
 
 		putpixel(dst, x, y, result);
 	}
