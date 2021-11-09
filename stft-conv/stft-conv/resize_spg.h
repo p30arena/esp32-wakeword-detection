1753411dc4d9f8d5f7b10c85286ca78d860af986
Ali EP <p30arena@gmail.com>
2021-11-09 10:14:21 +0330

    add resize to c code

--- stft-conv/stft-conv/resize_spg.h
+++ stft-conv/stft-conv/resize_spg.h
@@ -7,51 +7,50 @@ typedef struct
 	uint32_t h;
 } image_t;
 
-double getpixel(image_t *image, unsigned int x, unsigned int y)
-{
-	return image->pixels[(y * image->w) + x];
-}
+#define imgReadChannel(channel, width, r, c)        \
+    channel[(r) * (width) + (c)]
 
-float max(float a, float b) { return (a < b) ? a : b; };
-double lerp(double s, double e, double t) { return s + (e - s) * t; }
-double blerp(double c00, double c10, double c01, double c11, double tx, double ty)
-{
-	return lerp(lerp(c00, c10, tx), lerp(c01, c11, tx), ty);
-}
-
-void putpixel(image_t *image, unsigned int x, unsigned int y, double color)
-{
-	image->pixels[(y * image->w) + x] = color;
-}
+#define imgWriteChannel(channel, width, r, c, v)    \
+    (channel)[(r) * (width) + (c)] = (v)
 
 void resize(image_t *src, image_t *dst, int newWidth, int newHeight)
 {
-	const float xScale = (float)newWidth * src->w;
-	const float yScale = (float)newHeight * src->h;
-	int x, y;
-	for (x = 0, y = 0; y < newHeight; x++)
+	double           sr = 0.0;               // row scale
+	double           sc = 0.0;               // column scale
+	size_t          height = 0;
+	size_t          width = 0;
+
+	height = src->h;
+	width = src->w;
+	sr = (double)height / (double)newHeight;
+	sc = (double)width / (double)newWidth;
+
+
+	double rf = 0.0;
+	for (size_t rNew = 0; rNew < newHeight; rNew++, rf += sr)
 	{
-		if (x > newWidth)
+		size_t r = (size_t)rf;
+		r = (r > height - 2) ? height - 2 : r;
+		double deltaR = rf - r;
+		double oneMinusDeltaR = 1.0 - deltaR;
+
+		double cf = 0.0;
+		for (size_t cNew = 0; cNew < newWidth; cNew++, cf += sc)
 		{
-			x = 0;
-			y++;
+			size_t c = (size_t)cf;
+			c = (c > width - 2) ? width - 2 : c;
+			double deltaC = cf - c;
+			double w1 = oneMinusDeltaR * (1.0 - deltaC);
+			double w2 = deltaR * (1.0 - deltaC);
+			double w3 = oneMinusDeltaR * deltaC;
+			double w4 = deltaR * deltaC;
+
+			double v = imgReadChannel(src->pixels, width, r, c) * w1
+				+ imgReadChannel(src->pixels, width, r + 1, c) * w2
+				+ imgReadChannel(src->pixels, width, r, c + 1) * w3
+				+ imgReadChannel(src->pixels, width, r + 1, c + 1) * w4;
+
+			imgWriteChannel(dst->pixels, newWidth, rNew, cNew, v);
 		}
-
-		// Image should be clamped at the edges and not scaled.
-		//float gx = max(x / (float)(xScale - 0.5f), (float)(src->w - 1));
-		//float gy = max(y / (float)(yScale - 0.5f), (float)(src->h - 1));
-		float gx = x / (xScale + 0.5f);
-		float gy = y / (yScale + 0.5f);
-		int gxi = (int)gx;
-		int gyi = (int)gy;
-		double result = 0;
-		double c00 = getpixel(src, gxi, gyi);
-		double c10 = getpixel(src, gxi + 1, gyi);
-		double c01 = getpixel(src, gxi, gyi + 1);
-		double c11 = getpixel(src, gxi + 1, gyi + 1);
-
-		result = blerp(c00, c10, c01, c11, gx - gxi, gy - gyi);
-
-		putpixel(dst, x, y, result);
 	}
 }
